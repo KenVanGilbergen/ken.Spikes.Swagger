93e7356935715ab438d8928ac7cdeb9308c7c467
Ken Van Gilbergen <mail@ken.be>
2016-01-22 16:39:48 +0100

    Let's get started.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ken.Spikes.Swagger [WIP]
 Playing with Swagger in NodeJs and C#
+
+## NodeJs
+
+* npm install -g swagger
+* swagger project create ken-spikes-swagger
+* swagger project edit
+* npm test
+* npm start
+
+<h2>C#</h2>
+
+* Install-Package Swashbuckle
---
+++ ken-spikes-swagger/wallaby.js
@@ -0,0 +1,41 @@
+﻿module.exports = function () {
+    return {
+        files: [
+            'app.js',
+            'api/**/*.js',
+            { 'pattern' : 'api/**/*.yaml', 'instrument': false }
+        ],
+        
+        tests: [
+            'test/**/*.js'
+        ],
+        
+        env: {
+            type: 'node',
+            runner: 'node',
+            params: {
+                runner: '--harmony --harmony_arrow_functions'
+            }
+        },
+        "testFramework": "mocha",
+        bootstrap: function (wallaby) {
+            var runner = wallaby.testFramework;
+            //console.log(runner);
+            
+            //runner.ui('tdd');
+            runner.ui('bdd');
+            
+            runner.timeout(5000);
+            
+            // https://mochajs.org/#reporters
+            //runner.reporter('tap');
+            //runner.reporter('spec');
+            //runner.reporter('dot');
+            //runner.reporter('list');
+            //runner.reporter('progress');
+            //runner.reporter('nyan');
+            //runner.reporter('xunit');
+            runner.reporter('min');
+        }
+    };
+};
